04bbee602d7faff3e85aebbdfde2856e989296e6
Jon Ringvald <jvald8@gmail.com>
2015-05-06 15:01:19 -0700

    Modifying server.js to use express, addingexpress to package.json, updating html and css files

--- js/server.js
+++ js/server.js
@@ -1,47 +1,67 @@
 'use strict';
 
 var fs = require('fs');
-
 var http = require('http');
 var url  = require('url');
 
-function onRequest(request, response) {
-  console.log('calling the onRequest function');
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
 
-  var pathname = url.parse(request.url).pathname;
-
-  var pathnameLength = pathname.length;
-  console.log(pathname);
-  if (pathname === '/') {
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    var content = fs.readFileSync('../html/index.html');
-    response.write(content);
-  } else if (pathname === '/time') {
-    // Should always write the head of a response body first
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    // Creating a new date object and passing it to the
-    var currentTime = new Date();
-    response.write(currentTime.toDateString());
-  } else if (pathname.substring(0, 7) === '/greet/') {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    var greetLength = ('/greet/').length;
-    var greetThisName = pathname.substring(greetLength, pathnameLength);
-    response.write('Hello ' + greetThisName);
-  } else if (pathname.substring(0, 8) === '/signup') {
-    var content = fs.readFileSync('../html/signup.html');
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write(content);
-  } else if (pathname.substring(0, 7) === '/login') {
-    var content = fs.readFileSync('../html/login.html');
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write(content);
-  } else if (pathname) {
-    response.writeHead(404, {'Content-Type': 'text/plain'});
-    response.write('404 error: File not found.');
-  }
-  response.end();
+var madeMiddleware = function(request, response, next) {
+  console.log("From madeMiddleware:" + request.url);
+  next();
 }
 
-http.createServer(onRequest).listen(8000);
+var anotherMiddleware = function(request, response, next) {
+  console.log("From anotherMiddleware:" + request.url);
+  next();
+}
 
-console.log('Server has started');
+app.use(anotherMiddleware);
+
+app.use(madeMiddleware);
+
+app.get('/', function(request, response) {
+  var content = fs.readFileSync('../html/index.html');
+  response.set('Content-Type', 'text/html');
+  response.send(content);
+});
+
+app.get('/time', function(request, response) {
+  var currentTime = new Date();
+  response.set('Content-Type', 'text/plain');
+  response.send(currentTime.toDateString());
+});
+
+app.get('/login', function(request, response) {
+  var content = fs.readFileSync('../html/login.html');
+  response.set('Content-Type', 'text/html');
+  response.send(content);
+});
+
+app.get('/signup', function(request, response) {
+  var content = fs.readFileSync('../html/signup.html');
+  response.set('Content-Type', 'text/html');
+  response.send(content);
+});
+
+app.get('/css/homepage.css', function(request, response) {
+  var content = fs.readFileSync('../css/homepage.css');
+  response.set('Content-Type', 'text/css');
+  response.send(content);
+});
+
+app.get('/greet/:name/:title', function(request, response) {
+  console.log("greet pathname!")
+  response.set('Content-Type', 'text/plain');
+  response.send("Hey there " + request.params.title + ". " + request.params.name);
+});
+
+app.get('/*', function(request, response) {
+  response.status(404).send("File not found");
+});
+
+app.listen(process.env.PORT || 8000, function() {
+  console.log('server started');
+});
